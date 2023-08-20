af068710e374f6efb70a014718f6fde8b6973ce2
Iftach Naftaly <58637596+iftahnaf@users.noreply.github.com>
2023-08-20 09:40:24 +0300

    default bag saving in mcap (#28)
    * chaning qos profiles
    
    * using build_depend in px4_msgs
    
    * some minor cleanup
    
    * working configuration with mcap bags
    
    * saving bags in mcap

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -4,14 +4,9 @@ WORKSPACE_DIR="/workspaces/drone/"
 cd $WORKSPACE_DIR
 
 # Adding PX4 as submodule
-git submodule add https://github.com/PX4/PX4-Autopilot.git
-git submodule update --init --recursive
+git submodule add --force -b release/1.14 https://github.com/PX4/PX4-Autopilot.git
 cd PX4-Autopilot
-git checkout release/1.14
-cd ..
 git submodule update --init --recursive
-
-cd PX4-Autopilot
 
 # Installing all PX4 deps
 Tools/setup/ubuntu.sh --no-nuttx
@@ -24,7 +19,7 @@ cd ..
 cd ros2_ws
 colcon build
 cd ..
-mkdir /tmp/px4
+# mkdir /tmp/px4
 
 echo "
 # ==============================================
