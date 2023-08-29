23325a11c9c5641b05e79424785e9e133ead85b2
Iftach Nafatly <iftahnaf@gmail.com>
2023-08-29 11:33:08 +0000

    working configuration of the new px4 parameters handler

--- ros2_ws/src/px4_offboard/launch/px4_modifier.py
+++ ros2_ws/src/px4_offboard/launch/px4_modifier.py
@@ -13,6 +13,23 @@ class Modifier():
     _logger.setLevel(logging.INFO)
 
     @staticmethod
+    def _set_parameters(yaml_parameters: dict) -> dict:
+        lines_to_be_writter = {
+            "MC_PITCHRATE_P": f"param set-default MC_PITCHRATE_P {yaml_parameters['MC_PITCHRATE_P']}",
+            "MC_PITCHRATE_I": f"param set-default MC_PITCHRATE_I {yaml_parameters['MC_PITCHRATE_I']}",
+            "MC_PITCHRATE_D": f"param set-default MC_PITCHRATE_D {yaml_parameters['MC_PITCHRATE_D']}",
+            "MC_PITCHRATE_K": f"param set-default MC_PITCHRATE_K {yaml_parameters['MC_PITCHRATE_K']}",
+            "MC_PITCH_P": f"param set-default MC_PITCH_P: {yaml_parameters['MC_PITCH_P']}",
+
+            "MC_ROLLRATE_P": f"param set-default MC_ROLLRATE_P {yaml_parameters['MC_ROLLRATE_P']}",
+            "MC_ROLLRATE_I": f"param set-default MC_ROLLRATE_I {yaml_parameters['MC_ROLLRATE_I']}",
+            "MC_ROLLRATE_D": f"param set-default MC_ROLLRATE_D {yaml_parameters['MC_ROLLRATE_D']}",
+            "MC_ROLLRATE_K": f"param set-default MC_ROLLRATE_K {yaml_parameters['MC_ROLLRATE_K']}",
+            "MC_ROLL_P": f"param set-default MC_ROLL_P {yaml_parameters['MC_ROLL_P']}"
+        }
+        return lines_to_be_writter
+
+    @staticmethod
     def _parse_px4_yaml(yaml_file_path: str) -> dict:
         with open(yaml_file_path, "r") as stream:
             try:
@@ -38,44 +55,51 @@ class Modifier():
          return any(["PITCH" in x for x in lines])
     
     @staticmethod 
-    def _get_parameters_lines(lines: List[str]) -> List[int]:
-        stripped_lines = {}
-        parameters_to_be_changed = ["MC_PITCHRATE_P", 
-                          "MC_PITCHRATE_I", 
-                          "MC_PITCHRATE_D", 
-                          "MC_PITCHRATE_K", 
-                          "MC_ROLLRATE_P", 
-                          "MC_ROLLRATE_I", 
-                          "MC_ROLLRATE_D", 
-                          "MC_ROLLRATE_K",
-                          "MC_PITCH_P", 
-                          "MC_ROLL_P"]
+    def _get_parameters_lines(lines: List[str], lines_to_be_written: dict) -> dict:
+        parameters = {}
+        parameters_to_be_changed = lines_to_be_written.keys()
         for counter, line in enumerate(lines):
             try:
                 parameter = line.split("param set-default ")[1].split(" ")[0]
                 if parameter in parameters_to_be_changed:
                     value = line.split("param set-default ")[1].split(" ")[1]
-                    stripped_lines[parameter] = {"value": value, "line": counter+1}
+                    parameters[parameter] = {"value": value, "line": counter}
+                    Modifier._logger.info(f"Parameter {parameter} will be set to {value} in line {counter + 1}")
             except Exception:
                 pass
-        print(stripped_lines)
+        return parameters
 
     @classmethod
-    def change_px4_parameters(cls, parameter_file: str) -> None:
-        lines = cls._parse_px4_parameters(parameter_file)
-        if cls._check_for_existing_parameters(lines):
-            # print(lines)
-            parameters_lines = cls._get_parameters_lines(lines)
+    def change_px4_parameters(cls, parameter_file: str, yaml_file: str) -> None:
 
-        # else:
-        #     with open(parameter_file, "a") as file:
-        #         for line in lines:
-        #             file.write('\n' + line)
+        lines = cls._parse_px4_parameters(parameter_file)
+
+        yaml_parameters = cls._parse_px4_yaml(yaml_file)
+        lines_to_be_written = cls._set_parameters(yaml_parameters)
+
+        if cls._check_for_existing_parameters(lines):
+            # Parameters already exist in the file, change the value
+            parameters_lines = cls._get_parameters_lines(lines, lines_to_be_written)
+            for k,v in parameters_lines.items():
+                lines[v["line"]] = lines_to_be_written[k]
+            
+            with open(parameter_file, "w") as file:
+                for counter, line in enumerate(lines):
+                    if counter == 0:
+                        file.write(line)
+                    else:
+                        file.write("\n" + line)
+
+        else:
+            # Parameters not exist, append to the tail of the file
+            with open(parameter_file, "a") as file:
+                for k, v in lines_to_be_written.items():
+                    file.write('\n' + v)
          
 def main():
      parameter_file = '/workspaces/drone/ros2_ws/src/px4_offboard/launch/10015_gazebo-classic_iris'
      yaml_file = '/workspaces/drone/ros2_ws/src/px4_offboard/config/params.yaml'
-     Modifier.change_px4_parameters(parameter_file)
+     Modifier.change_px4_parameters(parameter_file, yaml_file)
 
 if __name__ == "__main__":
      main()
